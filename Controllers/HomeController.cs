9beafc99b5f183945a7167eb92cf5fe09d02747d
Bohdan Zaiats <bohdanzaiats999@.gmail.com>
2017-11-16 21:53:03 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+
+                //From Address  
+                string FromAddress = "bohdan2131@gmail.com";
+                string FromAdressTitle = "Email from ASP.NET Core 1.1";
+                //To Address  
+                string ToAddress = "bohdanzaiats999@gmail.com";
+                string ToAdressTitle = "Microsoft ASP.NET Core";
+                string Subject = "Hello World - Sending email using ASP.NET Core 1.1";
+                string BodyContent = "ASP.NET Core was previously called ASP.NET 5. It was renamed in January 2016. It supports cross-platform frameworks ( Windows, Linux, Mac ) for building modern cloud-based internet-connected applications like IOT, web apps, and mobile back-end.";
+
+                //Smtp Server  
+                string SmtpServer = "smtp.gmail.com";
+                //Smtp Port Number  
+                int SmtpPortNumber = 587;
+
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress(FromAdressTitle, FromAddress));
+                mimeMessage.To.Add(new MailboxAddress(ToAdressTitle, ToAddress));
+                mimeMessage.Subject = Subject;
+                mimeMessage.Body = new TextPart("plain")
+                {
+                    Text = BodyContent
+                };
+
+                using (var client = new SmtpClient())
+                {
+
+                    client.Connect(SmtpServer, SmtpPortNumber, false);
+                    // Note: only needed if the SMTP server requires authentication  
+                    // Error 5.5.1 Authentication   
+                    client.Authenticate("bohdan2131@gmail.com", "bohdan123");
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+                }
+            
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
